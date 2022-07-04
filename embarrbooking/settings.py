9ae183947a86324dcf83dca205d11fc004a31b48
chellej80 <embarrequestrian@gmail.com>
2022-07-04 20:43:18 +0000

    updated the secret key in settings

--- embarrbooking/settings.py
+++ embarrbooking/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q3)_r)2gm__(1^t%at^-*brxqe^vlm=aqta!byul$lvq5^#wol'
+SECRET_KEY = 'Akehcmbf2022##EmbarrEq!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
